ff3e189ad2e2705a407889d06a833b713c67cf98
Jace Lavigne <jacelavigne@student.usd214.org>
2020-10-06 14:09:50 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include "end.h"
+#include "dict.txt"
+#include "wordDistance.h"
+using namespace std;
+
+int main(){
+  wordDistance("hello", "names");
+  return 0;
+}
